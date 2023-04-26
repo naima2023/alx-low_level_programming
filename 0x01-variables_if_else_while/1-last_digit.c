030b340d7ed2842975068463e0ea4f773c4791bc
naima2023 <naimanaima1002@gmail.com>
2023-04-26 13:40:40 -0700

    first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ printf("Last digit of %d is %d and is 0\n", n, last);
 }
 else if (last < 6 && !last == 0)
 {
-printf("Last difit of %d is %d and is less than 6 and not 0\n", n, last);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 }
 return (0);
 
