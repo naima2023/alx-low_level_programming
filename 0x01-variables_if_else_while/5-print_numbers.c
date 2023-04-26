dcd8cf6adefd62900a2a9463e8e0ea0b4437926a
naima2023 <naimanaima1002@gmail.com>
2023-04-26 14:12:46 -0700

    first commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point 
+* Description: It prints all single digit numbers followed by new line
+* Return: the function returns 0
+*/
+int main(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
