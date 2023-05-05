b9a3bd680b740ed15a02369d4faad4e101af3dff
naima2023 <naimanaima1002@gmail.com>
2023-05-05 14:38:11 -0700

    pointer to variable 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_98 - updates rhe value of variable
+ * the pointer points to 98
+ * @n:point  to thevariable updates
+ */
+void reset_to_98(int *n)
+{
+	*n=98;
+}
+
