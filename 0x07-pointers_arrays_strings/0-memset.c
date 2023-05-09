45d06ff1c0da0ac3a2714c3ee9c6a58f15713a19
naima2023 <naimanaima1002@gmail.com>
2023-05-08 17:31:56 -0700

    0-mumset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,9 +8,11 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	 unsigned int i;
-	 for (i = 0;i < n ; i++)
+	 while ( i < n)
 	 {
-		 s[i] = b;
+		 *(s + i ) = b;
+
+		 i++;
 	 }
 	 return (s);
 }
