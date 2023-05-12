215e919ef3286a459287fecb58e66d94aba756b6
naima2023 <naimanaima1002@gmail.com>
2023-05-12 16:31:26 -0700

    initial

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/** 
+ * main - add positives numbers
+ * @argc :number of line command arguments
+ * @argv : array name
+ * Return :0
+ */
+
+int main ( int argc ,char *argv[])
+{
+int i, j, size,sum;
+char *ptr;
+
+if (argc < 2)
+printf("0\n");
+else
+{
+sum = 0;
+for (i = 1; i < argc; i++)
+{
+ptr = argv[i];
+length = strlen(ptr);
+
+for (j = 0; j < length; j++)
+{
+if (isdigit(*(ptr + j)) == 0)
+{
+printf("Error\n");
+return (1);
+}
+}
+
+sum += atoi(argv[i]);
+}
+
+printf("%d\n", sum);
+}
+return (0);
+}
+
