c14b52c1f2ecfd78e38039a1b606c91b2c5821b6
naima2023 <naimanaima1002@gmail.com>
2023-05-12 15:44:50 -0700

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include " stdio.h"
+
+/**
+ * main - prints numbers of arguments 
+ * @argc : Number pf arguments
+ * @argv Array name
+ * Return : 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf( "%s\n" , argc );
+
+return(0);
+}
+
