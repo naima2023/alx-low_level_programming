b99830395a2de7e70790b773433af468636c49c7
naima2023 <naimanaima1002@gmail.com>
2023-05-12 15:18:56 -0700

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * main - prints name of program
+ * @argc : number of command line arguments 
+ *@argv : array name
+ *Return : 0
+ */
+
+
+int main( int argc , char *argv[]){
+
+printf ("%s\n", argv[0]);
+
+return(0);
+
+}
+
