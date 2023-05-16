344fd08c623c90d9f44f8a845d5fdad042dbd0b0
naima2023 <naimanaima1002@gmail.com>
2023-05-16 15:02:28 -0700

    malloc-free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,30 +4,29 @@
 
 /**
  * create_array - array of char
- *  @size : size of array 
- *  @c: character
+ *@size : size of array 
+ *@c: character
  * Return : return a pointer to the array,or Null if it fail
  */
 
 char *create_array(unsigned int size, char c)
 {
+char *array = NULL;
+unsigned int i;
+if(size == 0)
+return(NULL);
 
-            char *array = NULL;
-	    unsigned int i;
-	   
-	    if(size == 0) 
-		    return(NULL);
+if(size != 0 )
+{
+ array = (char *)malloc(size * sizeof(char));
+if(array !=0)
+{
+for (i = 0;i < size ;i++)
+array[i] = c;
 
-	    if(size != 0 )
-		   {
-			   array = (char *)malloc(size * sizeof(char));
-			 if(array !=0)
-			 {	for (i = 0;i < size ;i++)
-					array[i] = c;
+}
+}
 
-			 }
-		   }
-
-			 return(array);
+return(array);
 }
 
