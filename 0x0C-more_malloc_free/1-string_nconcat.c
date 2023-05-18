f13df131db2e795ddc728a4709fcab0a8e156a35
naima2023 <naimanaima1002@gmail.com>
2023-05-18 16:03:36 -0700

    more malloc -free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,36 +11,35 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-{
-	unsigned int le = 0, i, a;
-	char *ptr;
+	unsigned int l1, i, e;
+	char *a;
 
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
+	l1 = 0;
+	while (s1[l1])
+		l1++;
 
-	while (s1[le])
-		le++;
+	a = malloc(sizeof(*a) * l1 + n + 1);
 
-	ptr  = malloc(sizeof(*ptr) * le + n + 1);
-
-	if (ptr == NULL)
+	if (a == NULL)
 		return (NULL);
 
-	for (i = 0, a  = 0; i < (l1 + n); i++)
+	for (i = 0, e = 0; i < (l1 + n); i++)
 	{
 		if (i < l1)
 		{
-			ptr[i] = s1[i];
+			a[i] = s1[i];
 		}
 		else
 		{
-	                ptr[i] = s2[a++];
+			a[i] = s2[e++];
 		}
 	}
-	ptr[i] = '\0';
-	return (ptr);
+	a[i] = '\0';
+	return (a);
 }
 
