42fd65f71c6a0cf1f676a375b47645048e134296
naima2023 <naimanaima1002@gmail.com>
2023-05-22 16:19:21 -0700

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
 /**
- * main -prints the name of the file it was compiled from
- * Return :all worked
- */
-int main (void)
+* main -prints the name of the file it was compiled from
+*
+* Return :all worked
+*/
+
+int main(void)
 {
 	printf( "%s\n", __BASE_FILE__);
 	return(0);
