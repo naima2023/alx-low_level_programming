e40c10e286c3c5cf027191f3901d5a657ce46c5f
naima2023 <naimanaima1002@gmail.com>
2023-05-22 15:37:18 -0700

    preprocessor

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,7 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
+#ifndef _object_like_macro_h_
+#define _object_like_macro_h_
+
 #define SIZE 1024
-#endif
+
+#endif/* _object_like_macro_h_ */
 
