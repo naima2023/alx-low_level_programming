753a1bf2a8c15b42ea5d4eca37cde5feb9ff29ca
naima2023 <naimanaima1002@gmail.com>
2023-05-27 16:10:21 -0700

    function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include "stdlib.h"
+
+/**
+ * print-name - print a name
+ * @name : namename
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL )
+		return f(name);
+}
+
