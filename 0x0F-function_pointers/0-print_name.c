7c0891e496ac81b29e217da9e1212157a1c23324
naima2023 <naimanaima1002@gmail.com>
2023-05-28 05:21:31 -0700

    function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,13 +3,15 @@
 
 /**
  * print-name - print a name
- * @name : namename
+ * @name : name
+ * @f : function pointer
+ *
+ * Return : no return 
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+	if (name && f)
+		f(name);
 }
 
